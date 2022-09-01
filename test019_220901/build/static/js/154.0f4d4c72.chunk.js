ee001ef33604ce8d388cc16a454aea464950b256
Cheolhyeon So <84296519+so0733@users.noreply.github.com>
2022-09-01 21:17:15 +0900

    Delete test019_220901/build directory

--- test019_220901/build/static/js/154.0f4d4c72.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest019_220901=self.webpackChunktest019_220901||[]).push([[154],{154:function(t,e,n){function u(){alert("\uc548\ub155\ud558\uc138\uc694!")}n.r(e),n.d(e,{default:function(){return u}})}}]);
-//# sourceMappingURL=154.0f4d4c72.chunk.js.map
